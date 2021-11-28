0775180fb26b6882011eb08f5df4d87ba5ad259b
Intrapreneur78078 <intrapreneur78078@gmail.com>
2021-11-28 20:37:02 +0530

    Lol

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    port: 5432,
-    user: "postgres",
-    password: "17.DEc.2003",
-    database: "smartbrain",
+    connectionString:process.env.DATABASE_URL,
+    ssl:true
   },
 });
 db.select("*")
